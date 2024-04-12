b2f1a3fb9d11dac5dc5bff06938948ea2d187b3e
lucaslomba <lucasazevedolomba@gmail.com>
2024-04-12 07:59:01 -0300

    Remove params

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
 
     if payment.paid:
-        return render_template('confirmed_payment.html', payment_id=payment.id, value=payment.value, host="http://127.0.0.1:5000", qr_code=payment.qr_code)
+        return render_template('confirmed_payment.html', payment_id=payment.id, value=payment.value)
 
     return render_template('payment.html', payment_id=payment.id, value=payment.value, host="http://127.0.0.1:5000", qr_code=payment.qr_code)
 
